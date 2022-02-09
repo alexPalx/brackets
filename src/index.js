43a341c3c88486d5230f628e399009225e653345
Alex Palx <alex.teanow@gmail.com>
2022-02-09 05:02:05 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+    str.split('').forEach((item, i) => {
+        const type = bracketsConfig.findIndex(brackets => brackets.includes(item));
+        if (item === bracketsConfig[type][1] && stack[stack.length - 1] === bracketsConfig[type][0])
+            stack.pop();
+        else stack.push(item);
+    });
+    return stack.length === 0;
 }
